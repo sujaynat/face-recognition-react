908e2ea41ec3e036b51e82a06ac5c4bb0f3251a6
Sujay Natarajan <sujaynat@Sujays-MBP.attlocal.net>
2024-10-06 00:20:21 -0500

    Added base facial recognition

--- src/components/Particles/Particles.js
+++ src/components/Particles/Particles.js
@@ -1,9 +1,6 @@
 import Particles, { initParticlesEngine } from "@tsparticles/react";
 import { useEffect, useMemo, useState } from "react";
-// import { loadAll } from "@/tsparticles/all"; // if you are going to use `loadAll`, install the "@tsparticles/all" package too.
-// import { loadFull } from "tsparticles"; // if you are going to use `loadFull`, install the "tsparticles" package too.
-import { loadSlim } from "@tsparticles/slim"; // if you are going to use `loadSlim`, install the "@tsparticles/slim" package too.
-// import { loadBasic } from "@tsparticles/basic"; // if you are going to use `loadBasic`, install the "@tsparticles/basic" package too.
+import { loadSlim } from "@tsparticles/slim"; 
 
 
 
@@ -13,11 +10,7 @@ const ParticlesComponent = (props) => {
   // this should be run only once per application lifetime
   useEffect(() => {
     initParticlesEngine(async (engine) => {
-      // you can initiate the tsParticles instance (engine) here, adding custom shapes or presets
-      // this loads the tsparticles package bundle, it's the easiest method for getting everything ready
-      // starting from v2 you can add only the features you need reducing the bundle size
-      //await loadAll(engine);
-      //await loadFull(engine);
+      
       await loadSlim(engine);
       //await loadBasic(engine);
     }).then(() => {
@@ -75,20 +68,20 @@ const ParticlesComponent = (props) => {
             default: "bounce",
           },
           random: true,
-          speed: 1,
+          speed: 3,
           straight: false,
         },
         number: {
           density: {
             enable: true,
           },
-          value: 150,
+          value: 250,
         },
         opacity: {
           value: 1.0,
         },
         shape: {
-          type: "circle",
+          type: "hexagon",
         },
         size: {
           value: { min: 1, max: 3 },
--- src/containers/App.js
+++ src/containers/App.js
@@ -5,17 +5,75 @@ import Navigation from '../components/Navigation/Navigation';
 import ImageLinkForm from '../components/ImageLinkForm/ImageLinkForm';
 import Rank from '../components/Rank/Rank';
 import ParticlesComponent from '../components/Particles/Particles';
+import FaceRecognition from '../components/FaceRecognition/FaceRecognition';
+
+
 
 class App extends Component {
+  constructor(){
+    super();
+    this.state = {
+      input: "",
+      imageUrl:"",
+      box: {}
+    }
+  }
+
+  displayFaceBox=(box) =>{
+    this.setState({box:box});
+  }
+
+  calculateFaceLocation = (response) =>{
+    const face = response.outputs[0].data.regions[0].region_info.bounding_box;
+    const image = document.getElementById("inputimage");
+    const width = Number(image.width);
+    const height = Number(image.height);
+    const faceBox ={
+      leftCol: face.left_col * width,
+      topRow: face.top_row * height,
+      rightCol: width - (face.right_col * width),
+      bottomRow: height - (face.bottom_row * height)
+    }
+    return faceBox;
+
+  }
+  onInputChange =(event) => {
+    this.setState({input: event.target.value})
+    console.log({input: event.target.value});
+  }
+
+  onButtonSubmit = () =>{
+    this.setState({imageUrl: this.state.input});
+    fetch('http://localhost:3000/imageurl', {
+      method: 'post',
+      headers: {'Content-Type': 'application/json'},
+      body: JSON.stringify({
+        input: this.state.input
+      })
+    })
+    .then(response=> response.json())
+    .then(response => {
+      console.log('hi', response)
+      this.displayFaceBox(this.calculateFaceLocation(response))
+    })
+    .catch(err => console.log(err));
+  }
+
+  
+
   render(){
+    const {imageUrl, box } = this.state;
     return(
       <div className='App'>
-        <ParticlesComponent id = "particles"/>
+        <ParticlesComponent className = "particles"/>
         <Navigation />
         <Logo />
         <Rank />
-        <ImageLinkForm />
-        {/*<FaceRecognition/>*/}
+        <ImageLinkForm
+                onInputChange={this.onInputChange}
+                onButtonSubmit={this.onButtonSubmit}
+              />
+        <FaceRecognition box={box} imageUrl = {imageUrl}/>
       </div>
     );
   }
