cb9b5cb9cd28d0f556da3f80c785d02ffa4772e2
Sujay Natarajan <sujaynat@Sujays-MBP.attlocal.net>
2024-11-05 17:51:59 -0600

    updating render backend url

--- src/containers/App.js
+++ src/containers/App.js
@@ -67,7 +67,7 @@ class App extends Component {
 
   onButtonSubmit = () =>{
     this.setState({imageUrl: this.state.input});
-    fetch('http://localhost:3000/imageurl', {
+    fetch('https://face-recognition-api-8lh5.onrender.com/imageurl', {
       method: 'post',
       headers: {'Content-Type': 'application/json'},
       body: JSON.stringify({
@@ -77,7 +77,7 @@ class App extends Component {
     .then(response => response.json())
     .then(response => {
       if (response) {
-        fetch('http://localhost:3000/image', {
+        fetch('https://face-recognition-api-8lh5.onrender.com/image', {
           method: 'put',
           headers: {'Content-Type': 'application/json'},
           body: JSON.stringify({
